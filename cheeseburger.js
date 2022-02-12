b2740b03269ffd63410e62518b37818e17a9ec50
0cticon <99093821+0cticon@users.noreply.github.com>
2022-02-12 14:06:41 -0600

    Update cheeseburger.js

--- cheeseburger.js
+++ cheeseburger.js
@@ -1,1 +1,7 @@
 
+function flipMe() {
+  Array.prototype.slice.call(document.querySelectorAll('body')).map(function(tag){tag.style['transform'] = 'rotate(' + 180 +'deg)';});
+}
+function unflipMe() {    
+  Array.prototype.slice.call(document.querySelectorAll('body')).map(function(tag){tag.style['transform'] = 'rotate(' + 0 +'deg)';});
+}
